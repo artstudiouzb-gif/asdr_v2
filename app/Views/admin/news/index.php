7715e03dee3d8eaaef9b5bbca0b1f7db20002d2e
artstudiouzb-gif <artstudiouzb@gmail.com>
2026-08-20 12:48:58 +0500

    feat(admin): move news import button and section into Settings (#208)
    * feat(admin): move news WXR/XML import button and section into Settings
    
    * fix(admin): remove external brand keyword in settings index text
    
    * fix(tests): restore rootHtaccess variable in wxr import test

--- app/Views/admin/news/index.php
+++ app/Views/admin/news/index.php
@@ -6,8 +6,7 @@ use App\Models\Language;
 
 $pageTitle = 'Новости';
 $activeNav = 'news';
-$pageActions = '<a href="/admin/news/import" class="btn">' . AdminUi::icon('database-import') . 'Импорт WXR/XML</a>'
-    . '<a href="/admin/news/create" class="btn btn--primary">' . AdminUi::icon('plus') . 'Добавить новость</a>';
+$pageActions = '<a href="/admin/news/create" class="btn btn--primary">' . AdminUi::icon('plus') . 'Добавить новость</a>';
 require __DIR__ . '/../layout/header.php';
 
 /** @var array $items */
